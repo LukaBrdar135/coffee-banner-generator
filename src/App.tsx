fa01593ebd80a56df07344c30c646e62ce341388
lukabrdar135 <luka.brdar135@gmail.com>
2023-10-26 23:09:26 +0200

    feat: header and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Welcome from './components/Welcome/Welcome'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='container'>
+      <Header />
+      <div className='page'>
+        <Welcome />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Welcome.css";
+
+const Welcome: React.FC = () => {
+    return (
+        <div className='welcome'>
+            <h2 className='welcome__title'>Welcome to the ThinkIT Coffee Banner Generator</h2>
+            <p className='welcome__description'>With this tool you are able to quickly craft your ideal coffee banner! Choose your blend and customize every detail for a a captivating brew display.</p>
+            <h3 className='welcome__action'>Simply complete the three easy steps below, and see your preview update as you go. Then view, copy and paste.</h3>
+        </div>
+    )
+}
+
+export default Welcome
